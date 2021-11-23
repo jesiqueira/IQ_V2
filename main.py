bd72144ffbae35a9171828267e45529dde380cf5
José Edmar de Siqueira <edmar.ade@gmail.com>
2021-11-23 06:36:53 -0300

    refatorado o metodo de entrada

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import json
 # PRACTICE / REAL
 api = IQ_Option(Config.LOGIN, Config.PASSWORD, active_account_type='PRACTICE')
 
-PAR = 'EURUSD-OTC'
+# PAR = 'EURUSD-OTC'
+PAR = 'EURUSD'
 ENTRADA = 'call'  # put/call
 TIMEFRAME = 60  # time em segunddo, 60 segundo igual 1 minuto
 
@@ -34,7 +35,7 @@ else:
 
 # candle = api.get_candles(PAR, TIMEFRAME, 10, time.time())
 # print(candle[-2]['close'])
-sinais = Sinais(api)
+sinais = Sinais(PAR, api, TIMEFRAME)
 # candles = sinais.candles(api, PAR, TIMEFRAME, 50, time.time())
 # indicador = Indicadores(candles)
 # print(f'SMA9: {indicador.sma(9)}')
@@ -45,45 +46,48 @@ sinais = Sinais(api)
 
 
 # while True:
-    # vela = api.get_realtime_candles(PAR, 60)
-    # candles = sinais.candles(PAR, TIMEFRAME, 50, time.time())
-    # indicador = Indicadores(candles)
-    # smaRapida = indicador.sma(9)
-    # smaLenta = indicador.sma(25)
-    # for velas in vela:
-    #     try:
-    #     # print(f'smaRapida: {smaRapida}')
-    #     # print(f'smaLenta: {smaLenta}')
-    #     # print(vela[velas]['close'])
-    #     	sinais.entrada(smaRapida, smaLenta, vela[velas]['close'])
-    #     except RuntimeError:
-    #         print('Erro')
+# vela = api.get_realtime_candles(PAR, 60)
+# candles = sinais.candles(PAR, TIMEFRAME, 50, time.time())
+# indicador = Indicadores(candles)
+# smaRapida = indicador.sma(9)
+# smaLenta = indicador.sma(25)
+# for velas in vela:
+#     try:
+#     # print(f'smaRapida: {smaRapida}')
+#     # print(f'smaLenta: {smaLenta}')
+#     # print(vela[velas]['close'])
+#     	sinais.entrada(smaRapida, smaLenta, vela[velas]['close'])
+#     except RuntimeError:
+#         print('Erro')
 
-    # time.sleep(1)
+# time.sleep(1)
+# print(api.get_positions)
+
 api.start_candles_stream(PAR, 60, 1)
 time.sleep(1)
 while True:
-	with open('startStop.json', 'r') as file:
-		data = json.load(file)
-		if not int(data['ligado']):
-			break
-		else:
-			try:
-				vela = api.get_realtime_candles(PAR, 60)
-				candles = sinais.candles(PAR, TIMEFRAME, 50, time.time())
-				indicador = Indicadores(candles)
-				smaRapida = indicador.sma(9)
-				smaLenta = indicador.sma(25)
+    with open('startStop.json', 'r') as file:
+        data = json.load(file)
+        if not int(data['ligado']):
+            break
+        else:
+            try:
+                vela = api.get_realtime_candles(PAR, 60)
+                # candles = sinais.candles(50, time.time())
+                # indicador = Indicadores(candles)
+                # smaRapida = indicador.sma(9)
+                # smaLenta = indicador.sma(25)
 
-				for velas in vela:
-					try:
-					# print(f'smaRapida: {smaRapida}')
-					# print(f'smaLenta: {smaLenta}')
-					# print(vela[velas]['close'])
-						sinais.entrada(smaRapida, smaLenta, vela[velas]['close'])
-					except RuntimeError:
-						print('Erro')
-			except RuntimeError:
-				pass
+                for velas in vela:
+                    try:
+                        # print(f'smaRapida: {smaRapida}')
+                        # print(f'smaLenta: {smaLenta}')
+                        # print(vela[velas]['close'])
+                        # sinais.entrada(smaRapida, smaLenta, vela[velas]['close'])
+                        # sinais.entrada(PAR, vela[velas]['close'], time.time())
+                        sinais.entrada(vela[velas]['close'], time.time())
+                    except RuntimeError:
+                        print('Erro')
+            except RuntimeError:
+                pass
 api.stop_candles_stream(PAR, 60)
-
--- padroes/sinais.py
+++ padroes/sinais.py
@@ -1,26 +1,49 @@
+from os import path
 from indicadores.indicadores import Indicadores
 
+
 class Sinais:
-    
-    def __init__(self, api):
+
+    def __init__(self, par, api, timeframe):
+        self.par = par
         self.api = api
-    
-    def candles(self, par, timeframe, qtdVelas, realtime):
+        self.timeframe = timeframe
+
+    def candles(self, qtdVelas, timeAtual):
         ''' 
+            Pegar histórico dos candles
             Par = moeda negociada;
             timeframe = o time frame das velas em segundos 60 segundos igual 1 minuto;
             qtdVelas = quantidade de velas a carregar;
-            realtime = é o minuto corrente em timestamp;
+            realtime = é o minuto corrente em timestamp => time.time();
         '''
-        return self.api.get_candles(par, timeframe, qtdVelas, realtime)
+        return self.api.get_candles(self.par, self.timeframe, qtdVelas, timeAtual)
         # print(candle[-2]['close'])
 
-    
-    def entrada(self, smaRapida, smaLenta, candleAtual):
+    def entrada(self, candleAtual, timeAtual, smaRapida=9, smaLenta=34, qtdVelas=50):
+        '''
+            Recebe os parametros e verifica se atende os padões de entrada;
+            candleAtual -> Ultimo clandle;
+            timeAtual -> ultimo time ou minuto atual, time.time()
+            sma... -> para calcula a média
+            qtdVelas = quantas velas vão ser carregadas no histórico
+        '''
+        candles = self.candles(qtdVelas, timeAtual)
+        Indicador = Indicadores(candles)
+        smaRapida = Indicador.sma(smaRapida)
+        smaLenta = Indicador.sma(smaLenta)
         if candleAtual < smaRapida < smaLenta:
-            print('put') 
+            # realizar venda ou PUT
+            print('put')
         elif candleAtual > smaRapida > smaLenta:
-            print('call')
-
-        
-
+            # Realizar compra ou CALL
+            # print(candles[-2])
+            print(candles[-2]['close'])
+            maior = []
+            for candle in candles[-50 : -2]:
+                maior.append(candle['close'])
+                # print(f"vela: {n}, maxima: {l['close']}")
+            ma = max(maior)
+            print(f'Max close: {ma}')
+        else:
+            print('Preço entre as médias')
