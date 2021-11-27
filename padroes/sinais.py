fce809d25265958c398e8d7f6d21e177b7fdcfea
José Edmar de Siqueira <edmar.ade@gmail.com>
2021-11-27 11:33:15 -0300

    Modificado padrão de entrada

--- padroes/sinais.py
+++ padroes/sinais.py
@@ -22,7 +22,7 @@ class Sinais:
         return self.api.get_candles(self.par, self.timeframe, qtdVelas, timeAtual)
         # print(candle[-2]['close'])
 
-    def entrada(self, candleAtual, timeAtual, smaRapida=7, smaLenta=34, qtdVelas=10):
+    def entrada(self, candleAtual, timeAtual, smaRapida=7, smaLenta=100, qtdVelas=110):
         '''
             Recebe os parametros e verifica se atende os padões de entrada;
             candleAtual -> Ultimo clandle;
@@ -33,50 +33,81 @@ class Sinais:
         candles = self.candles(qtdVelas, timeAtual)
         Indicador = Indicadores(candles)
         smaRapida = Indicador.sma(smaRapida)
+        smaLenta = Indicador.sma(smaLenta)
+        qtd_candle_p_max_min = 20
 
         # smaLenta = Indicador.sma(smaLenta)
-        if candleAtual < smaRapida:
+        if candleAtual < smaRapida <smaLenta:
             # realizar venda ou PUT
             trader = Trader(self.api)
-            # print(candles[-2]['close'])
-            # print(candles)
             # print(f'Minima close: {self.minima(candles)}')
             # if candles[-2]['open'] > candles[-2]['close'] > candles[-1]['min'] < self.minima(candles):
             print('put')
-            if candles[-2]['open'] > candles[-2]['close'] and candles[-1]['open'] > candles[-1]['close'] and candles[-1]['open'] < smaRapida > candles[-1]['close'] and candles[-1]['min'] < self.minima(candles):
+            # print(f'Open 2: {candles[-3]["open"]}')
+            # print(f'close 2: {candles[-3]["close"]}')
+            # print(f'Open 1: {candles[-2]["open"]}')
+            # print(f'close 1: {candles[-2]["close"]}')
+            print(f"Máxima: {self.maxima(qtd_candle_p_max_min, timeAtual)}")
+            print(f"Minima: {self.minima(qtd_candle_p_max_min, timeAtual)}")
+            print(f"candle[-1] {candles[-2]['close']}")
+            if candleAtual < self.minima(qtd_candle_p_max_min, timeAtual):
+                print('Esperando 7 segundos')
+                sleep(7)
+            # if candles[-2]['open'] > candles[-2]['close'] and candles[-1]['open'] > candles[-1]['close'] and candles[-1]['open'] < smaRapida > candles[-1]['close'] and candles[-1]['min'] < self.minima(candles):
+            if candles[-3]['open'] > candles[-3]['close'] and candles[-2]['open'] > candles[-2]['close'] and candles[-1]['open'] < smaRapida \
+                and candles[-1]['close'] < self.minima(qtd_candle_p_max_min, timeAtual) and candleAtual < candles[-2]['close'] \
+                and candleAtual < self.minima(qtd_candle_p_max_min, timeAtual):
+                print('Compra Efetuada')
                 status, id = trader.put(1, self.par, 3)
                 if status:
                     resutado, lucro = self.api.check_win_v3(id)
                     print(f'Resultado: {resutado}, lucro: {lucro}')
-                    sleep(30)
+                    sleep(20)
 
-        elif candleAtual > smaRapida:
+        elif candleAtual > smaRapida > smaLenta:
             # Realizar compra ou CALL
-            # print(candles[-2])
             trader = Trader(self.api)
-            # print(candles[-2]['close'])
-            # print(candles)
             # print(f'Max close: {self.maxima(candles)}')
             # if candles[-2]['open'] < candles[-2]['close'] < candles[-1]['max'] > self.maxima(candles):
             print('call')
-            if candles[-2]['open'] < candles[-2]['close'] and candles[-1]['open'] < candles[-1]['close'] and candles[-1]['open'] > smaRapida < candles[-1]['close'] and candles[-1]['max'] > self.maxima(candles):
+            # print(f'Open 2: {candles[-3]["open"]}')
+            # print(f'close 2: {candles[-3]["close"]}')
+            # print(f'Open 1: {candles[-2]["open"]}')
+            # print(f'close 1: {candles[-2]["close"]}')
+            print(f"Máxima: {self.maxima(qtd_candle_p_max_min, timeAtual)}")
+            print(f"Minima: {self.minima(qtd_candle_p_max_min, timeAtual)}")
+            print(f"candle[-1] {candles[-2]['close']}")
+            if candleAtual > self.maxima(qtd_candle_p_max_min, timeAtual):
+                print('Esperando 7 segundos')
+                sleep(7)
+            # if candles[-2]['open'] < candles[-2]['close'] and candles[-1]['open'] < candles[-1]['close'] and candles[-1]['open'] > smaRapida < candles[-1]['close'] and candles[-1]['max'] > self.maxima(candles):
+            if candles[-3]['open'] < candles[-3]['close'] and candles[-2]['open'] < candles[-2]['close'] and candles[-1]['open'] > smaRapida \
+                and candles[-1]['close'] > self.maxima(qtd_candle_p_max_min, timeAtual) and candleAtual > candles[-2]['close'] \
+                and candleAtual > self.maxima(qtd_candle_p_max_min, timeAtual):
+                print('Compra Efetuada')
                 status, id = trader.call(1, self.par, 3)
                 if status:
                     resutado, lucro = self.api.check_win_v3(id)
                     print(f'Resultado: {resutado}, lucro: {lucro}')
-                    sleep(30)
+                    sleep(20)
         else:
             print('Preço entre as médias')
+            # print(f'Open 2: {candles[-3]["open"]}')
+            # print(f'close 2: {candles[-3]["close"]}')
+            # print(f'Open 1: {candles[-2]["open"]}')
+            # print(f'close 1: {candles[-2]["close"]}')
 
-    def maxima(self, candles):
+    def maxima(self, qtd_velas, time_atual):
+        candles = self.candles(qtd_velas, time_atual)
         maxima = []
-        for candle in candles[: -1]:
+        for candle in candles[ : -1]:
             maxima.append(candle['close'])
         return max(maxima)
 
-    def minima(self, candles):
+    def minima(self, qtd_velas, time_atual):
+        candles = self.candles(qtd_velas, time_atual)
         minima = []
-        for candle in candles[: -1]:
+        for candle in candles[ : -1]:
             minima.append(candle['close'])
         return min(minima)
 
